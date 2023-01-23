06306589c0f01d20159f5ec3aa56b8ab448446ed
Nihilistzsche <nihilistzsche@gmail.com>
2023-01-22 23:03:20 -0800

    Fixes

--- migrations/items.lua
+++ migrations/items.lua
@@ -1,7 +1,10 @@
 local Items = {items = {}}
 
-for scienceName in pairs(data.raw.tool) do
-	if scienceName:sub(1, 6) == "sem:spg" then
+local function has_valid_prefix(name, prefix) return name:sub(1, string.len(prefix)) == prefix end
+
+for scienceName in pairs(data and data.raw.tool or {}) do
+	if has_valid_prefix(scienceName, "sem:spg") then
+		log(scienceName)
 		Items.items[scienceName] = { type = "tool" }
 	end
 end
